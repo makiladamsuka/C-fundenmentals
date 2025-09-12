ff4e97b01d252f98ae7b68d2917f614b3079d448
makiladamsuka <makiladamsuka09@gmail.com>
2025-09-12 07:23:33 +0530

    rotating matrixes

--- 12.c
+++ 12.c
@@ -7,7 +7,17 @@ void main(){
                      {7, 8, 9}};
     int arr2[3][3];
     int arr3[3][3];
-                    
+
+
+    printf("Original matrix\n");            
+    for (int row=0; row<3;row++){
+        for (int col=0; col<3; col++){
+            printf("%d \t", arr[row][col]);
+         
+        }
+    printf("\n");
+    }
+
 /*      Expected output
         1  4  7                       7  4  1                 
         2  5  8        -->            8  5  2
@@ -31,10 +41,26 @@ for (int row=0; row<3;row++){
 }
 printf("\n");
 
-printf("reverse each rows\n");
+printf("reverse each rows --> clockwise\n");
 for (int row=0; row<3;row++){
     for (int col=0; col<3; col++){
         arr3[row][col] = arr2[row][2-col];
+         
+    }
+    
+}
+for (int row=0; row<3;row++){
+    for (int col=0; col<3; col++){
+        printf("%d \t", arr3[row][col]);
+         
+    }
+    printf("\n");
+}
+
+printf("reverse each rows --> countercloackwise\n");
+for (int row=0; row<3;row++){
+    for (int col=0; col<3; col++){
+        arr3[row][col] = arr2[2-row][col];
          
     }
     
