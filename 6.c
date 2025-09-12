f2c3691f379ddaba1f89810de29343a237eedadc
makiladamsuka <makiladamsuka09@gmail.com>
2025-09-12 06:59:43 +0530

    add two numbers with pointers

--- 6.c
+++ 6.c
@@ -1,17 +1,20 @@
 //add two numbers with pointers
 #include<stdio.h>
 
+int add(int *,int *);
+
 int main(){
     int num1, num2, *pnum1, *pnum2, sum;
 
     num1 = 1;
     num2 = 2;
 
-    pnum1 = &num1;
-    pnum2 = &num2;
+ 
 
-    sum = *pnum1 + *pnum2;
+    printf("sum: %d", add(&num1, &num2));
 
-    printf("sum: %d", sum);
+}
 
+int add(int *ptr1, int *ptr2){
+    return *ptr1 + *ptr2;
 }
